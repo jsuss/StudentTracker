9890826039fd6048dba32ed6c9416dd175ca71b6
jsuss <Jesus.Arredondo@hologic.com>
2021-04-14 01:33:28 -0700

    Add project files.

---
+++ StudentTracker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StudentTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
