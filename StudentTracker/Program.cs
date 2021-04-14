a6fc8d8757371e376566c83729654f3a866c9b11
Jesus Arredondo <arredondo16.jesus@gmail.com>
2021-04-14 01:51:38 -0700

    Final changes

--- StudentTracker/Program.cs
+++ StudentTracker/Program.cs
@@ -8,24 +8,44 @@ namespace StudentTracker
         static void Main(string[] args)
         {
             var students = new List<Student>();
+            // While value is true, users are able to add more students
             bool adding = true;
 
             while(adding)
             {
+                // Create new instance of Student
                 Student newStudent = new Student();
 
                 Console.Write("Enter name: ");
                 newStudent.Name = Console.ReadLine();
 
-                students.Add(newStudent);
+                Console.Write("Enter grade: ");
+                newStudent.Grade = int.Parse(Console.ReadLine());
 
+                Console.Write("Enter birthday: ");
+                newStudent.Birthday = Console.ReadLine();
+
+                Console.Write("Enter address: ");
+                newStudent.Address = Console.ReadLine();
+
+                Console.Write("Enter name: ");
+                newStudent.Phone = long.Parse(Console.ReadLine());
+
+                // Add new instance of Student to students list
+                students.Add(newStudent);
+                
                 Console.WriteLine("Add another student? y/n");
+                // Check if user decided to stop adding users
                 if (Console.ReadLine() != "y")
                     adding = false;
             }
 
+            // Extra line for formatting
+            Console.WriteLine();
+
+            // Display all students' information
             foreach (Student student in students)
-                Console.WriteLine("Name: {0}", student.Name);
+                Console.WriteLine("Name: {0} - Grade: {1}", student.Name, student.Grade);
         }
     }
     class Student
