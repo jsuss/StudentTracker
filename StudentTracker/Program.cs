cd0a16a5fd6f13935bb80b9aaab98c5966875fc1
Jesus Arredondo <arredondo16.jesus@gmail.com>
2021-04-15 22:36:51 -0700

    Adding school enumerator

--- StudentTracker/Program.cs
+++ StudentTracker/Program.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 
 namespace StudentTracker
 {
+    enum School
+    {
+        Hogwarts,
+        Harvard,
+        MIT
+    }
     class Program
     {
+        static List<Student> students = new List<Student>();
+
         static void Main(string[] args)
         {
-            var students = new List<Student>();
             bool addStudents = true;
 
             while (addStudents)
@@ -17,7 +24,8 @@ namespace StudentTracker
                     Student newStudent = new Student();
 
                     newStudent.Name = Util.Console.Ask("Enter name: ");
-                    newStudent.Grade = Util.Console.AskInt("Enter grade: ");            
+                    newStudent.Grade = Util.Console.AskInt("Enter grade: ");
+                    newStudent.School = (School) Util.Console.AskInt("Enter school: (type the corresponding number):\n 0: Hogwarts\n 1: Harvard\n 2: MIT\n");
                     newStudent.Birthday = Util.Console.Ask("Enter birthday: ");
                     newStudent.Address = Util.Console.Ask("Enter address: ");
                     newStudent.Phone = Util.Console.AskLong("Enter phone number: ");
@@ -44,12 +52,32 @@ namespace StudentTracker
             foreach (Student student in students)
                 Console.WriteLine("Name: {0} - Grade: {1} - Phone: {2}", student.Name, student.Grade, student.Phone);
 
+            Exports();
+
             Console.WriteLine("Student Count: {0}", Student.Count);
         }
         static void Import()
         {
             var importedStudent = new Student("Jesus", 98, "November", "301 Vermont", 7603909370);
             Console.WriteLine(importedStudent.Name);
+        }
+        static void Exports()
+        {
+            foreach(var student in students)
+            {
+                switch(student.School)
+                {
+                    case School.Hogwarts:
+                        Console.WriteLine("Exporting to Hogwarts");
+                        break;
+                    case School.Harvard:
+                        Console.WriteLine("Exporting to Harvard");
+                        break;
+                    case School.MIT:
+                        Console.WriteLine("Exporting to MIT");
+                        break;
+                }
+            }
         }
     }
     class Member
@@ -68,6 +96,7 @@ namespace StudentTracker
         static public int Count = 0;
         public int Grade;
         public string Birthday;
+        public School School;
         public Student() { }
         public Student(string name, int grade, string birthday, string address, long phone)
         {
